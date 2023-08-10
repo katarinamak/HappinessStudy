c72cecace16d7ed22046556b6a52596d18f2980f
katarinamakivic <kaya.makivic@gmail.com>
2023-08-09 18:42:04 -0700

    added world happiness data to project file

--- happiness_study.py
+++ happiness_study.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot
 
 # read from a csv file
-df = pd.read_csv("/Users/katarinamakivic/Downloads/World Happiness data/2019.csv")
+df = pd.read_csv("./WorldHappinessData/2019.csv")
 otherdf = pd.read_csv("/Users/katarinamakivic/Downloads/Refactored_Py_DS_ML_Bootcamp-master/09-Geographical-Plotting"
                       "/2014_World_GDP")
 
@@ -28,9 +30,25 @@ fig = go.Figure(data=go.Choropleth(
     colorbar={'title': 'Happiness Score'}
 ))
 
-
 # layout variable
 fig.update_layout(title='2019 World Happiness Study',
                   geo=dict(showframe=False,
                            projection={'type': 'equirectangular'}))
-fig.show()
+
+# fig.show()
+
+plt.figure(0)
+sns.pairplot(df)
+plt.show()
+
+variables = ['GDP per capita', 'Social support', 'Healthy life expectancy', 'Freedom to make life choices',
+             'Generosity', 'Perceptions of corruption']
+
+correlations = []
+for i in variables:
+    correlations.append(df['Score'].corr(df[i]))
+
+print("The following table shows the correlation between the happiness score of a country and all the other factors "
+      "considered. The goal of this is to see which factors have the greatest impact on a nation's happiness.")
+corr_table = pd.DataFrame(correlations, variables, columns=['correlation'])
+print(corr_table)
